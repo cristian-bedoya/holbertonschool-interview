64238583a508109003864c15abaa61a1863dc8ed
cristian-bedoya <crisbed321@gmail.com>
2022-02-24 23:47:25 +0000

    add project  Ox0A

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,13 @@
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT 2
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif
