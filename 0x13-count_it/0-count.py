6a4e9c11539ff57d16612b71fcef4cd65c63e073
cristian-bedoya <crisbed321@gmail.com>
2022-05-26 23:52:30 +0000

    Fix errors

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,62 +1,47 @@
 #!/usr/bin/python3
-"""Count it problem
-"""
-import requests
+""" module docs """
+from requests import request
 
 
-def recurse(subreddit, word_list, hot_titles, after=""):
-    """Recursive function that queries the Reddit API and
-    prints a sorted count of given keywords
-    Args:
-        subreddit: string representing subreddit to search for
-        word_list: collection of keywords to search in the
-        subreddit
-        hot_titles: dictionary of number of occurrences of elements
-        in word_list that are found in the request
-        after: pagination feature of the reddit api
+def generate_dicts(word_list):
+    """ method docs """
+    count = {k: 0 for k in word_list}
+    dup = {}
+    for k in word_list:
+        if k not in dup:
+            dup[k] = 0
+        dup[k] += 1
+    return (count, dup)
 
-    Return:
-        hot_titles or None
-    """
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                 after)
-    res = requests.get(url,
-                       headers={'User-agent': 'product'},
-                       allow_redirects=False)
 
-    if res.status_code != 200:
+def count_words(subreddit, word_list, after="", count={}, dup={}, init=0):
+    """ method docs """
+    if not init:
+        count, dup = generate_dicts(word_list)
+
+    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
+    headers = {"User-Agent": "Python3"}
+    response = request("GET", url, headers=headers).json()
+    try:
+        data = response.get('data')
+        top = data.get('children')
+        _after = data.get('after')
+
+        for item in top:
+            data = item.get('data')['title']
+            for word in count:
+                amount = data.lower().split(' ').count(word.lower())
+                count[word] += amount
+
+        if _after:
+            count_words(subreddit, word_list, _after, count, dup, 1)
+        else:
+            sort_abc = sorted(count.items(), key=lambda tup: tup[::-1])
+            desc = sorted(sort_abc, key=lambda tup: tup[1], reverse=True)
+
+            for name, cnt in desc:
+                cnt *= dup[name]
+                if cnt:
+                    print('{}: {}'.format(name.lower(), cnt))
+    except Exception:
         return None
-    if after is None:
-        return hot_titles
-
-    for i in res.json().get('data').get('children'):
-        title_s = i.get('data').get('title').split()
-        for word in set(word_list):
-            if word.lower() in [x.lower() for x in title_s]:
-                if hot_titles.get(word):
-                    hot_titles[word] += 1
-                else:
-                    hot_titles[word] = 1
-
-    after = res.json().get('data').get('after')
-    recurse(subreddit, word_list, hot_titles, after)
-    return hot_titles
-
-
-def count_words(subreddit, word_list):
-    """Calls recurse to get dictionary of ocurrences of word_list
-    and prints that dictionary sorted by values in decreasing order
-
-    Args:
-        subreddit: string representing subreddit to search for
-        word_list: collection of keywords to search in the
-        subreddit
-    Return:
-        each keyword with its corresponding count in order
-    """
-    hot_titles = recurse(subreddit, word_list, {})
-    if hot_titles:
-        for k, v in sorted(hot_titles.items(), key=lambda val: val[1],
-                           reverse=True):
-            if v != 0:
-                print('{}: {}'.format(k, v))
--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -1,8 +1,5 @@
-#ifndef HEAP_EXTRACT
-#define HEAP_EXTRACT
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -20,11 +17,11 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
+#include <stdlib.h>
+
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
-
-void binary_tree_print(const binary_tree_t *tree);
-
+void binary_tree_print(const binary_tree_t *);
 int heap_extract(heap_t **root);
 
-#endif
+#endif /* _BINARY_TREES_H_ */
