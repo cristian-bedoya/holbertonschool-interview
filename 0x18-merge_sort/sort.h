3f2d52e2cd0c30a388ce0bb4852652eef438182c
cristian-bedoya <crisbed321@gmail.com>
2022-06-10 21:19:24 +0000

    set changes

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -1,10 +1,9 @@
-#ifndef MERGE_S
-#define MERGE_S
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
-void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
